342fb5aa7c0d59d21bdc32acf98cfcef78d9ddd4
Kseniia_Viushkova <kseniia_viushkova@epam.com>
2019-04-10 17:39:25 +0300

    add functionality from project YuliaAtlasova/YandexSpellerTests

---
+++ src/main/java/core/YandexSpellerCheckTextApi.java
@@ -0,0 +1,93 @@
+package core;
+
+import beans.YandexSpellerAnswer;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import core.constants.YandexSpellerConstants;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.Matchers.lessThan;
+
+public class YandexSpellerCheckTextApi {
+
+
+    public static final String YANDEX_SPELLER_API_URI =
+            "https://speller.yandex.net/services/spellservice.json/checkText";
+    private HashMap<String, String> params = new HashMap<>();
+
+    //builder pattern
+    private YandexSpellerCheckTextApi() {
+    }
+
+    public static ApiBuilder with() {
+        YandexSpellerCheckTextApi api = new YandexSpellerCheckTextApi();
+        return new ApiBuilder(api);
+    }
+
+    //get ready Speller answers list form api response
+    public static List<YandexSpellerAnswer> getYandexSpellerAnswers(Response response) {
+        return new Gson().fromJson(response.asString().trim(), new TypeToken<List<YandexSpellerAnswer>>() {
+        }.getType());
+    }
+
+    //set base request and response specifications tu use in tests
+    public static ResponseSpecification successResponse() {
+        return new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectHeader("Connection", "keep-alive")
+                .expectResponseTime(lessThan(20000L))
+                .expectStatusCode(HttpStatus.SC_OK)
+                .build();
+    }
+
+    public static RequestSpecification baseRequestConfiguration() {
+        return new RequestSpecBuilder()
+                .setAccept(ContentType.XML)
+                .setRelaxedHTTPSValidation()
+                .addHeader("custom header2", "header2.value")
+                .addQueryParam("requestID", new Random().nextLong())
+                .setBaseUri(YANDEX_SPELLER_API_URI)
+                .build();
+    }
+
+    public static class ApiBuilder {
+        YandexSpellerCheckTextApi spellerApi;
+
+        private ApiBuilder(YandexSpellerCheckTextApi gcApi) {
+            spellerApi = gcApi;
+        }
+
+        public ApiBuilder text(String text) {
+            spellerApi.params.put(YandexSpellerConstants.PARAM_TEXT, text);
+            return this;
+        }
+
+        public ApiBuilder options(String options) {
+            spellerApi.params.put(YandexSpellerConstants.PARAM_OPTIONS, options);
+            return this;
+        }
+
+        public ApiBuilder language(YandexSpellerConstants.Language language) {
+            spellerApi.params.put(YandexSpellerConstants.PARAM_LANG, language.languageCode);
+            return this;
+        }
+
+        public Response callApi() {
+            return RestAssured.with()
+                    .queryParams(spellerApi.params)
+                    .log().all()
+                    .get(YANDEX_SPELLER_API_URI).prettyPeek();
+        }
+    }
+}
---
+++ src/main/java/core/constants/YandexSpellerConstants.java
@@ -0,0 +1,53 @@
+package core.constants;
+
+public class YandexSpellerConstants {
+
+    //useful constants for API under test
+    public static final String PARAM_TEXT = "text";
+    public static final String PARAM_OPTIONS = "options";
+    public static final String PARAM_LANG = "lang";
+    public static final String QUOTES = "\"";
+
+
+    public enum Language {
+        RU("ru"),
+        UK("uk"),
+        EN("en");
+        public String languageCode;
+
+        Language(String lang) {
+            this.languageCode = lang;
+        }
+    }
+
+    public enum ErrorCodes {
+        ERROR_UNKNOWN_WORD("1"),
+        ERROR_REPEAT_WORD("2"),
+        ERROR_CAPITALIZATION("3"),
+        ERROR_TOO_MANY_ERRORS("4");
+
+        private String code;
+
+        ErrorCodes(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+
+    public enum CorrectTexts {
+        RUSSIAN("Этот текст на русском языке"),
+        URL_TEXT("This text contains URL http://yandex.ru");
+        private String text;
+
+        CorrectTexts(String text) {
+            this.text = text;
+        }
+
+        public String text() {
+            return text;
+        }
+    }
+}
